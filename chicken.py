4f127ce1a44261836cd7b49726da3ec73f98ddc3
lehanh <taovanluc45@gmail.com>
2026-08-23 22:57:36 +0700

    Add Chicken class with feeding functionality

--- chicken.py
+++ chicken.py
@@ -1,1 +1,18 @@
+class Chicken:
 
+    def __init__(self):
+        self.name = "Gà con"
+        self.level = 1
+        self.exp = 0
+        self.food = 100
+        self.coin = 0
+
+
+    def feed(self):
+        self.food += 10
+        self.exp += 5
+
+
+        if self.exp >= 100:
+            self.level += 1
+            self.exp = 0
